f2b20e44eea8d7ecc548f1736123795fdf132fcd
Maciej <piontuso@gmail.com>
2017-11-14 19:06:22 +0100

    apigility app

---
+++ data/statuslib.php
@@ -0,0 +1,9 @@
+<?php
+return array(
+    '58cf69d2-4a62-4f4b-941f-b3fb335503d5' => array(
+        'message' => 'First post!',
+        'user' => 'mwop',
+        'id' => '58cf69d2-4a62-4f4b-941f-b3fb335503d5',
+        'timestamp' => 1510682262,
+    ),
+);
